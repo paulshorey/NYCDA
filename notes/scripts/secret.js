f0c052e78308d38015d3447347618bdc091ce284
paul (local) <ps@artspaces.net>
2016-12-09 00:59:04 -0700

    try again - defer script

--- notes/scripts/secret.js
+++ notes/scripts/secret.js
@@ -1,36 +1,33 @@
-(function(){
-	var converter = new showdown.Converter();
+var converter = new showdown.Converter();
 
-	/* 
-		loop through notes 
-	*/
-	var notes = ["git", "javascript_links", "terminal", "terminal_vim"];
-	for (var n in notes) {
-		var name = notes[n];
+/* 
+	loop through notes 
+*/
+var notes = ["git", "javascript_links", "terminal", "terminal_vim"];
+for (var n in notes) {
+	var name = notes[n];
 
-		// 1. add placeholders
-		$( "#notes_keys" ).append( "<li note=\"#"+name+"\">Tab 1</li>" );
-		$( "#notes_values" ).append( "<div id=\""+name+"\">" );
+	// 1. add placeholders
+	$( "#notes_keys" ).append( "<li note=\"#"+name+"\">Tab 1</li>" );
+	$( "#notes_values" ).append( "<div id=\""+name+"\">" );
 
-		// 2. add content into each placeholder
-		$.get( ""+name+".md", function( text ) {
-			$( "#notes_value_"+name ).html( converter.makeHtml(text) );
-		});
-
-	}
-
-	/*
-		animate the tabs
-	*/
-	$("#notes_keys > * ").click(function(event) {
-		var tab = $(this).attr("note");
-
-		$(this).addClass("current");
-		$(this).siblings().removeClass("current");
-
-		$("#notes_values > * ").not(tab).css("display", "none");
-		$("#notes_values > #"+tab).fadeIn();
-
+	// 2. add content into each placeholder
+	$.get( ""+name+".md", function( text ) {
+		$( "#notes_value_"+name ).html( converter.makeHtml(text) );
 	});
 
-}());
+}
+
+/*
+	animate the tabs
+*/
+$("#notes_keys > * ").click(function(event) {
+	var tab = $(this).attr("note");
+
+	$(this).addClass("current");
+	$(this).siblings().removeClass("current");
+
+	$("#notes_values > * ").not(tab).css("display", "none");
+	$("#notes_values > #"+tab).fadeIn();
+
+});
