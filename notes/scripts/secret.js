756f450d659f132ed8f49c272b320165e43c40c5
paul (local) <ps@artspaces.net>
2016-12-09 00:55:54 -0700

    lets see if this works

---
+++ notes/scripts/secret.js
@@ -0,0 +1,36 @@
+(function(){
+	var converter = new showdown.Converter();
+
+	/* 
+		loop through notes 
+	*/
+	var notes = ["git", "javascript_links", "terminal", "terminal_vim"];
+	for (var n in notes) {
+		var name = notes[n];
+
+		// 1. add placeholders
+		$( "#notes_keys" ).append( "<li note=\"#"+name+"\">Tab 1</li>" );
+		$( "#notes_values" ).append( "<div id=\""+name+"\">" );
+
+		// 2. add content into each placeholder
+		$.get( ""+name+".md", function( text ) {
+			$( "#notes_value_"+name ).html( converter.makeHtml(text) );
+		});
+
+	}
+
+	/*
+		animate the tabs
+	*/
+	$("#notes_keys > * ").click(function(event) {
+		var tab = $(this).attr("note");
+
+		$(this).addClass("current");
+		$(this).siblings().removeClass("current");
+
+		$("#notes_values > * ").not(tab).css("display", "none");
+		$("#notes_values > #"+tab).fadeIn();
+
+	});
+
+}());
